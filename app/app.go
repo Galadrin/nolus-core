0e670e8cc759929032ab73c5c1d5a94a8db2ed19
David Vennik <david@cybriq.systems>
2022-05-29 12:45:27 +0300

    Added wasm proposals
    They are now available but a lot of unused things in here I presume will be used for the secondary part of this issue

--- app/app.go
+++ app/app.go
@@ -153,6 +153,38 @@ func getGovProposalHandlers() []govclient.ProposalHandler {
 }
 
 var (
+	// WasmProposalsEnabled enables Wasm proposals:
+	// If EnableSpecificWasmProposals is "", and this is "true", then enable all x/wasm proposals.
+	// If EnableSpecificWasmProposals is "", and this is not "true", then disable all x/wasm proposals.
+	WasmProposalsEnabled = "true"
+
+	// EnableSpecificWasmProposals If set to non-empty string it must be comma-separated list of values that are all a subset
+	// of "EnableAllProposals" (takes precedence over WasmProposalsEnabled)
+	// https://github.com/CosmWasm/wasmd/blob/02a54d33ff2c064f3539ae12d75d027d9c665f05/x/wasm/internal/types/proposal.go#L28-L34
+	EnableSpecificWasmProposals = ""
+
+	// use this for clarity in argument list
+	EmptyWasmOpts []wasm.Option
+)
+
+// GetWasmEnabledProposals parses the WasmProposalsEnabled / EnableSpecificWasmProposals values to
+// produce a list of enabled proposals to pass into wasmd app.
+func GetWasmEnabledProposals() []wasm.ProposalType {
+	if EnableSpecificWasmProposals == "" {
+		if WasmProposalsEnabled == "true" {
+			return wasm.EnableAllProposals
+		}
+		return wasm.DisableAllProposals
+	}
+	chunks := strings.Split(EnableSpecificWasmProposals, ",")
+	proposals, err := wasm.ConvertToProposals(chunks)
+	if err != nil {
+		panic(err)
+	}
+	return proposals
+}
+
+var (
 	// DefaultNodeHome default home directories for the application daemon
 	DefaultNodeHome string
 
