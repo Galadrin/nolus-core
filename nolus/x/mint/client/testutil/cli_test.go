ccf21a9d95436761e56f2be980eee3e90b4d859e
David Vennik <david@cybriq.systems>
2022-05-31 13:35:06 +0300

    Added the actual regenerated 😅

---
+++ nolus/x/mint/client/testutil/cli_test.go
@@ -0,0 +1,13 @@
+package testutil
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"gitlab-nomo.credissimo.net/nomo/cosmzone/nolus/testutil/network"
+)
+
+func TestIntegrationTestSuite(t *testing.T) {
+	cfg := network.DefaultConfig()
+	suite.Run(t, NewIntegrationTestSuite(cfg))
+}
---
+++ nolus/x/mint/simulation/abci_test.go
@@ -0,0 +1,39 @@
+package simulation_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	"gitlab-nomo.credissimo.net/nomo/cosmzone/nolus/app/params"
+	"gitlab-nomo.credissimo.net/nomo/cosmzone/nolus/testutil/simapp"
+	"gitlab-nomo.credissimo.net/nomo/cosmzone/nolus/x/mint"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_BeginBlock(t *testing.T) {
+	params.SetAddressPrefixes()
+	app, err := simapp.TestSetup()
+	if err != nil {
+		t.Errorf("Error while creating simapp: %v\"", err)
+	}
+	blockTime := time.Now()
+	header := tmproto.Header{Height: app.LastBlockHeight() + 1}
+	ctx := app.BaseApp.NewContext(false, header).WithBlockTime(blockTime)
+	minterKeeper := app.MintKeeper
+	mint.BeginBlocker(ctx, minterKeeper)
+	header = tmproto.Header{Height: app.LastBlockHeight() + 2}
+	ctx2 := ctx.WithBlockHeader(header).WithBlockTime(blockTime.Add(time.Second * 40))
+	mint.BeginBlocker(ctx2, minterKeeper)
+	minter := minterKeeper.GetMinter(ctx2)
+	feeCollector := app.AccountKeeper.GetModuleAccount(ctx2, types.FeeCollectorName)
+	feesCollectedInt := app.BankKeeper.GetAllBalances(ctx2, feeCollector.GetAddress())
+	feesCollected := sdk.NewDecCoinsFromCoins(feesCollectedInt...)
+	fmt.Printf("norm %v, total %v \n", minter.NormTimePassed, minter.TotalMinted)
+	fmt.Printf("balance %v \n", feesCollected)
+	require.Equal(t, minter.TotalMinted, feesCollectedInt.AmountOf(sdk.DefaultBondDenom))
+}
